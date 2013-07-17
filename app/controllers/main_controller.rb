cac40dc5eb321bfb6a465def673d66ab632931f4
Fraser Murray <fraser.m.murray@gmail.com>
2013-07-17 21:41:04 +0100

    Fixed the filter turning on erroneously

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -8,12 +8,14 @@ class MainController < ApplicationController
   def ceiling
     @first_hero, @second_hero = *Hero.random(2)
     @mode = "upper"
+    @matchup_mode = "upper"
     render "home"
   end
 
   def floor
     @first_hero, @second_hero = *Hero.random(2)
     @mode = "lower"
+    @matchup_mode = "lower"
     render "home"
   end
 
@@ -25,7 +27,7 @@ class MainController < ApplicationController
     @first_hero = Hero.find params[:winner]
     @second_hero = Hero.find params[:loser]
     @mode = params[:mode]
-    matchup_mode = params[:matchup_mode]
+    @matchup_mode = params[:matchup_mode]
     if @first_hero && @second_hero && ["upper", "lower"].include?(@mode)
       if @mode == "upper"
         @first_hero.upper.beats @second_hero.upper
@@ -41,9 +43,9 @@ class MainController < ApplicationController
     else
       puts "Invalid result!!"
     end
-    if matchup_mode == "upper"
+    if @matchup_mode == "upper"
       redirect_to "/ceiling"
-    elsif matchup_mode == "lower"
+    elsif @matchup_mode == "lower"
       redirect_to "/floor"
     else
       redirect_to root_path
