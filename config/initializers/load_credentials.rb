5c229c0d001bb8d37d86ba4d98026b277c3f07e2
Fraser Murray <fraser.m.murray@gmail.com>
2013-07-17 22:38:28 +0100

    Removed the (local-only) load_credentials file

--- config/initializers/load_credentials.rb
+++
@@ -1,2 +0,0 @@
-ENV["CREDENTIALS_PASSWORD"] = YAML.load_file("#{Rails.root}/config/credentials.yml")["password"]
-ENV["CREDENTIALS_USERNAME"] = YAML.load_file("#{Rails.root}/config/credentials.yml")["username"]
