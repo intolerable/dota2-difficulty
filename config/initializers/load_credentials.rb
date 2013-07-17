073b45daaf04fa17c6d80a4778ce9715503fb0ca
Fraser Murray <fraser.m.murray@gmail.com>
2013-07-17 22:35:31 +0100

    Fixed auth to use correct ENV variables in heroku

--- config/initializers/load_credentials.rb
+++ config/initializers/load_credentials.rb
@@ -1,1 +1,2 @@
-CREDENTIALS = YAML.load_file("#{Rails.root}/config/password.yml")
+ENV["CREDENTIALS_PASSWORD"] = YAML.load_file("#{Rails.root}/config/credentials.yml")["password"]
+ENV["CREDENTIALS_USERNAME"] = YAML.load_file("#{Rails.root}/config/credentials.yml")["username"]
